9bbd2e77bc9566a4312d18109aad920085e263a0
Christian <nelson.christian.m@gmail.com>
2019-05-09 20:16:06 -0400

    added more tests

--- main.cpp
+++ main.cpp
@@ -5,5 +5,7 @@ int main() {
   PointCloud pc(
       "/Users/christiannelson/Documents/LiDAR-tools/test-data/mingo.las");
   pc.printHeader();
+  pc.printHeadXYZ(5);
+  pc.printTailXYZ(8);
   return 0;
 }
