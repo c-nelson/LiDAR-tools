7a9e2b0cf89b7c39ea4cfaf0c7331e2639574132
Christian Nelson <ChristianN@bme.local>
2019-05-10 14:56:55 -0400

    added method defs to work on in future

--- PointCloud.hpp
+++ PointCloud.hpp
@@ -19,8 +19,12 @@ class PointCloud {
   void printHeader();
   void printHead(int n, bool longVersion = false);
   void printTail(int n, bool longVersion = false);
-  std::vector<Point> getRadiusN(int n, double x, double y, double z);
-  std::vector<Point> convert2Ft(std::vector<Point> intialPoints);
-  std::vector<Point> convert2Meters(std::vector<Point> intial Points);
-  std::vector<Point> sortByClass(std::vector<Point> intial Points);
+  std::vector<Point> getRadius(double radius, double x, double y, double z);
+  std::vector<Point> convert2Ft(const std::vector<Point> &intialPoints);
+  std::vector<Point> convert2Meters(const std::vector<Point> &intialPoints);
+  std::vector<Point> sortByClass(const std::vector<Point> &intialPoints);
+  void outputASCII(string path);
+  voud outputLAS(string path);
+  std::vector<Point> removeAbnormalZValues(double radius, double x, double y,
+                                           double z);
 };
